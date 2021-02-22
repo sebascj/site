f21f987ebd9b9579b54f6cbc195498e4fa7501ec
sebastian <sebas.cj@gmail.com>
2021-02-22 09:26:24 -0500

    Home page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hey there</h1>
+      <h3>This is a work in progress home page</h3>
+    </div>
+  );
+};
+
+render(<App />, document.getElementById('root'));
