7ab52309c1c02f162a8b6f67c0fe1388c30886f9
sebastian <sebas.cj@gmail.com>
2021-02-23 10:39:01 -0500

    removed unused files, updated package-lock.json

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
+import Head from 'next/head';
+
 function HomePage() {
   return (
     <div>
+      <Head>
+        <title>Home | Sebastian</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
       <h1>Hey There, </h1>
       <h2>This is work in progress home page</h2>
       <h3>Currently using NextJS</h3>
