d4d37729e8070c590142602e0430d4eadf62a83f
sebastian <sebas.cj@gmail.com>
2021-02-23 09:43:37 -0500

    Added: Next.js

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+function HomePage() {
+  return (
+    <div>
+      <h1>Hey There, </h1>
+      <h2>This is work in progress home page</h2>
+      <h3>Currently using NextJS</h3>
+    </div>
+  );
+}
+
+export default HomePage;
