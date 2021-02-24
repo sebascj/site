504daa988c38f8a599ea86f91f4bc77c953c1e8c
sebastian <sebas.cj@gmail.com>
2021-02-23 20:32:03 -0500

    initial animation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,13 @@
 import Head from 'next/head';
+import { useEffect, useRef } from 'react';
+import oscillator from './components/canvas.controller';
 
 function HomePage() {
+  const oscillatorCanvas = useRef();
+  useEffect(() => {
+    const canvasContext = oscillatorCanvas.current.getContext('2d');
+    oscillator(false, canvasContext);
+  });
   return (
     <div>
       <Head>
@@ -10,6 +17,7 @@ function HomePage() {
       <h1>Hey There, </h1>
       <h2>This is work in progress home page</h2>
       <h3>Currently using NextJS</h3>
+      <canvas ref={oscillatorCanvas}></canvas>
     </div>
   );
 }
