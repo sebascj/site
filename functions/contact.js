5a03b4032535a3bd59b4aa827b0a6d1f21faaf56
sebastian <sebas.cj@gmail.com>
2021-02-25 01:24:54 -0500

    added stupid deprecated module

--- functions/contact.js
+++ functions/contact.js
@@ -2,24 +2,24 @@ const mailgun = require('mailgun-js');
 
 // const formData = new FormData();
 // const mailgun = new Mailgun(formData);
-// const mg = mailgun.client({
-//   username: 'api',
-//   key: process.env.MAILGUN_API_KEY
-// });
 
-exports.handler = async function (event, context) {
-  // await mg.messages
-  //   .create('sandbox48da53d19a2b4316ae55ec1e025f472f.mailgun.org', {
-  //     from: 'Sebastian Web Developer <sebas.cj@gmail.com>',
-  //     to: ['sebas.cj@gmail.com'],
-  //     subject: 'Hello World',
-  //     message: '<h1>Testing some Mailgun awesomness</h1>'
-  //   })
-  //   .catch((error) => {
-  //     console.error(error);
-  //   });
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'email sent' })
-  };
+exports.handler = function (event, context) {
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_API_KEY,
+    domain: process.env.MAILGUN_URL
+  });
+  mg.messages().send(
+    {
+      from: 'Sebastian Web Developer <sebas.cj@gmail.com>',
+      to: ['sebas.cj@gmail.com'],
+      subject: 'Hello World',
+      text: '<h1>Testing some Mailgun awesomness</h1>'
+    },
+    (error, response) => {
+      console.log({
+        statusCode: 200,
+        body: JSON.stringify({ message: 'email sent' })
+      });
+    }
+  );
 };
