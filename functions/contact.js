1d5d4fdb6ef5284bc2c31a899735d0852ea82ddd
sebastian <sebas.cj@gmail.com>
2021-02-25 02:44:55 -0500

    logs

--- functions/contact.js
+++ functions/contact.js
@@ -5,6 +5,8 @@ exports.handler = function (event, context, callback) {
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_URL
   });
+  console.log(mg);
+  console.log('mg created');
   const data = {
     from: 'Name <something@YOUR_DOMAIN>',
     to: 'sebas.cj@gmail.com',
@@ -12,7 +14,12 @@ exports.handler = function (event, context, callback) {
     text: 'TEXT',
     html: 'HTML'
   };
+  console.log(data);
+
   mg.messages().send(data, (error, response) => {
+    console.log('message send');
+    console.log(error);
+    console.log(response);
     if (error) {
       return console.log(error);
     }
