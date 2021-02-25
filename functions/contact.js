9c4c788c224ee398a48e9b0d244618d27a6ec4d5
sebastian <sebas.cj@gmail.com>
2021-02-24 23:53:39 -0500

    removed try-catch

--- functions/contact.js
+++ functions/contact.js
@@ -10,21 +10,18 @@ const mg = mailgun.client({
 });
 
 exports.handler = async function (event, context) {
-  try {
-    await mg.messages.create(
-      'sandbox48da53d19a2b4316ae55ec1e025f472f.mailgun.org',
-      {
-        from: 'Sebastian Web Developer <sebas.cj@gmail.com>',
-        to: ['sebas.cj@gmail.com'],
-        subject: 'Hello World',
-        message: '<h1>Testing some Mailgun awesomness</h1>'
-      }
-    );
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'email sent' })
-    };
-  } catch (error) {
-    console.error(error);
-  }
+  await mg.messages
+    .create('sandbox48da53d19a2b4316ae55ec1e025f472f.mailgun.org', {
+      from: 'Sebastian Web Developer <sebas.cj@gmail.com>',
+      to: ['sebas.cj@gmail.com'],
+      subject: 'Hello World',
+      message: '<h1>Testing some Mailgun awesomness</h1>'
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: 'email sent' })
+  };
 };
