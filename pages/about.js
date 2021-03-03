72a8c9262c9cbef639e48d512118894f740e5951
sebastian <sebas.cj@gmail.com>
2021-03-02 21:54:03 -0500

    fixed about text

--- pages/about.js
+++ pages/about.js
@@ -33,30 +33,30 @@ function About() {
           <AboutContent>
             <AboutArticle>
               <Paragraph>
-                My name is Sebastian Clavijo and I am a frontend developer at
+                My name is Sebastian Clavijo, and I am a frontend developer at
                 Anomali, where I have worked for the last 4 years.
               </Paragraph>
               <Paragraph>
-                I fell in love with programming when I was studying for a
+                I fell in love with programming when I studied for a
                 bachelor&apos;s degree in Electronic Engineering and programmed
-                my first microcontroller in assembly language. Since then I have
-                been passionate about coding, algorithms, and logic.
+                my first microcontroller in assembly language. Since then, I
+                have been passionate about coding, algorithms, and logic.
               </Paragraph>
               <Paragraph>
                 In my last year at university, two classmates and I built the
-                winning robot in a robotics competition that was held in
-                Medellin, the city where I live.
+                winning robot in a robotics competition held in Medellin, the
+                city where I live.
               </Paragraph>
               <Paragraph>
                 I worked with government educational programs teaching robotics
-                and programming to girls and boys in their final years of
-                school. It was a very important part of my life that taught me
-                the importance of transmitting my knowledge to others.
+                and programming to girls and boys in their final school years.
+                It was a significant part of my life that taught me the
+                importance of transmitting my knowledge to others.
               </Paragraph>
               <Paragraph>
                 When I&apos;m not coding, I spend my free time with my loved
                 ones or losing games in World of Warcraft. I like building my
-                own computers, playing guitar and video games.
+                own computers, playing guitar, photography and video games.
               </Paragraph>
               <Paragraph>
                 The game that moved me the most: The last of us part II
