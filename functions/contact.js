9aae8eea6831f0d4d6c215ff7aef6e2d00ff1ad9
Sebastian Clavijo <sebas.cj@gmail.com>
2021-03-08 21:36:16 -0500

    Enabled contact form (#10)
    * Enabled contact form
    
    * Added async to netlify function

--- functions/contact.js
+++ functions/contact.js
@@ -1,25 +1,24 @@
 const mailgun = require('mailgun-js');
 
-exports.handler = function (event, context, callback) {
+exports.handler = async function (event) {
+  const message = JSON.parse(event.body);
   const mg = mailgun({
     apiKey: process.env.MAILGUN_API_KEY,
     domain: process.env.MAILGUN_URL
   });
-  const data = {
-    from: 'Name <sebas.cj@gmail.com>',
-    to: 'sebas.cj@gmail.com',
-    subject: 'SUBJECT',
-    text: 'TEXT',
-    html: 'HTML'
+  const email = {
+    from: `${message.name} <${message.email}>`,
+    to: `Sebastian Clavijo <${process.env.EMAIL}>`,
+    subject: message.subject,
+    text: message.message
   };
-
-  mg.messages().send(data, (error, response) => {
-    if (error) {
-      return console.log(error);
-    }
-    callback(null, {
+  try {
+    await mg.messages().send(email);
+    return {
       statusCode: 200,
-      body: 'Mail sent'
-    });
-  });
+      body: 'Mail Sent'
+    };
+  } catch (e) {
+    return { statusCode: 500, body: `Error trying to send message: ${e}` };
+  }
 };
