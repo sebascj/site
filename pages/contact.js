d0cf430ad0f41560bbe9bfbaae2f1b0f560bff47
sebastian <sebas.cj@gmail.com>
2021-03-10 17:59:33 -0500

    added social media icons to contact page

--- pages/contact.js
+++ pages/contact.js
@@ -5,6 +5,7 @@ import Layout from '../components/layout/layout';
 import Form from '../components/form/Form';
 import GoogleMap from '../components/map/GoogleMap';
 import { Title } from '../components/font-elements/Fonts';
+import Icon from '../components/icon/Icon';
 
 const FormWrapper = styled.div`
   position: relative;
@@ -25,6 +26,22 @@ const ContactWrapper = styled.div`
     margin: 4em;
   }
 `;
+const ContactIcons = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+  height: 4rem;
+  cursor: pointer;
+  a {
+    text-decoration: none;
+    margin: 0.4em;
+  }
+  span::before {
+    color: var(--text-gray);
+    font-size: 1.5em;
+  }
+`;
 
 function About() {
   return (
@@ -37,6 +54,26 @@ function About() {
           <FormWrapper>
             <Title>Contact me</Title>
             <Form />
+            <ContactIcons>
+              <a
+                href="https://www.linkedin.com/in/sebastian-clavijo/"
+                rel="noreferrer"
+                target="_blank">
+                <Icon name="linkedin"></Icon>
+              </a>
+              <a
+                href="https://github.com/sebascj"
+                rel="noreferrer"
+                target="_blank">
+                <Icon name="github"></Icon>
+              </a>
+              <a
+                href="https://twitter.com/sh4k_the_kid"
+                rel="noreferrer"
+                target="_blank">
+                <Icon name="twitter"></Icon>
+              </a>
+            </ContactIcons>
           </FormWrapper>
           <GoogleMap />
         </ContactWrapper>
